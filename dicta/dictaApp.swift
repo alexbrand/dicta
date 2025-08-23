18274b787202a16e02af79bb4952661a25d1f460
Alexander Brand <alexbrand09@gmail.com>
2025-08-23 10:36:55 -0400

    Initial Commit

---
+++ dicta/dictaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dictaApp.swift
+//  dicta
+//
+//  Created by Alex Brand on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct dictaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dictaTests/dictaTests.swift
@@ -0,0 +1,17 @@
+//
+//  dictaTests.swift
+//  dictaTests
+//
+//  Created by Alex Brand on 8/23/25.
+//
+
+import Testing
+@testable import dicta
+
+struct dictaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
